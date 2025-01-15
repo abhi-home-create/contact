8413c8729531602bbe4a15beea1c7cad265b7bc6
abhi-home-create <yadavabhi0143@gmail.com>
2025-01-15 13:24:00 +0530

    version2.1

---
+++ src/js/config/config.js
@@ -0,0 +1,3 @@
+export const CONFIG = {
+    SCRIPT_ID: '__GOOGLE_SCRIPT_ID__' // This will be replaced during deployment
+};
--- src/js/config/environment.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    SCRIPT_ID: '__GOOGLE_SCRIPT_ID__'
-};
-
-export default CONFIG;
